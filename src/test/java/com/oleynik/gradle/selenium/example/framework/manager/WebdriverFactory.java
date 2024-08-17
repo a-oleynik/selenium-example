6113d1d4fa9d8ba8b535b757f53f5cc3c646454f
Andrey Oleynik <andrei.oleynik@gmail.com>
2024-08-17 15:48:20 +0200

    switched to Selenium Manager

--- src/test/java/com/oleynik/gradle/selenium/example/framework/manager/WebdriverFactory.java
+++ src/test/java/com/oleynik/gradle/selenium/example/framework/manager/WebdriverFactory.java
@@ -15,24 +15,14 @@ public class WebdriverFactory {
         WebDriver driver;
         String chosenBrowser = configuration().envBrowser().toLowerCase();
         driver = switch (chosenBrowser) {
-            case "firefox" -> {
-                System.setProperty("webdriver.gecko.driver", ".\\drivers\\geckodriver.exe");
-                yield new FirefoxDriver();
-            }
+            case "firefox" -> new FirefoxDriver();
             case "chrome" -> {
-                System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--disable-search-engine-choice-screen");
                 yield new ChromeDriver(options);
             }
-            case "ie" -> {
-                System.setProperty("webdriver.ie.driver", ".\\drivers\\IEDriverServer.exe");
-                yield new InternetExplorerDriver();
-            }
-            case "edge" -> {
-                System.setProperty("webdriver.edge.driver", ".\\drivers\\msedgedriver.exe");
-                yield new EdgeDriver();
-            }
+            case "ie" -> new InternetExplorerDriver();
+            case "edge" -> new EdgeDriver();
             default -> throw new IllegalStateException("Browser is not supported");
         };
 
