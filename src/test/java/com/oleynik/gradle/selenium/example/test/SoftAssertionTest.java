7f5305ef0151a782ed3e82164abdab07d6a43ddb
Andrii Olieinik <andrei.oleynik@gmail.com>
2026-07-19 19:50:00 +0200

    fix: Fix work with calculator.com (update locators, remove adds, scrall element into view before click). Add scrollToElementCenter method to WebdriverUtils. Disabled SoftAssertionTest

--- src/test/java/com/oleynik/gradle/selenium/example/test/SoftAssertionTest.java
+++ src/test/java/com/oleynik/gradle/selenium/example/test/SoftAssertionTest.java
@@ -5,23 +5,25 @@ import com.oleynik.gradle.selenium.example.steps.CalculatorSteps;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.oleynik.gradle.selenium.example.framework.config.ConfigurationManager.configuration;
 
+@Disabled
 @Feature("Soft assertions")
 public class SoftAssertionTest extends BaseTest {
     private CalculatorSteps calculatorSteps;
     private final String url = configuration().environmentUrl();
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         calculatorSteps = new CalculatorSteps();
     }
 
     @Test
     @Description("Test with soft assertions")
-    public void checkSoftAssertions() {
+    void checkSoftAssertions() {
         calculatorSteps.openCalculator(url);
         calculatorSteps.checkSoftAsserts();
     }
